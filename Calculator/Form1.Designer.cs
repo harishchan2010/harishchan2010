35e6531ba697520c8149ef17fb6ce2b527745c86
Sivakumar <Sivakumar@DESKTOP-9FK3OJK>
2021-08-22 18:27:01 +0530

    Add project files.

---
+++ Calculator/Form1.Designer.cs
@@ -0,0 +1,187 @@
+﻿
+namespace Calculator
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Input1 = new System.Windows.Forms.TextBox();
+            this.Input2 = new System.Windows.Forms.TextBox();
+            this.Output = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.clear_button = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(28, 41);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(135, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Enter First Number:";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(-2, 83);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(165, 20);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Enter Sencond Number:";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(111, 247);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(52, 20);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "Result:";
+            // 
+            // Input1
+            // 
+            this.Input1.Location = new System.Drawing.Point(174, 38);
+            this.Input1.Name = "Input1";
+            this.Input1.Size = new System.Drawing.Size(265, 27);
+            this.Input1.TabIndex = 3;
+            this.Input1.KeyPress += new System.Windows.Forms.KeyPressEventHandler(this.Input1_KeyPress);
+            // 
+            // Input2
+            // 
+            this.Input2.Location = new System.Drawing.Point(174, 80);
+            this.Input2.Name = "Input2";
+            this.Input2.Size = new System.Drawing.Size(265, 27);
+            this.Input2.TabIndex = 4;
+            this.Input2.TextChanged += new System.EventHandler(this.Input2_TextChanged);
+            // 
+            // Output
+            // 
+            this.Output.Location = new System.Drawing.Point(174, 244);
+            this.Output.Name = "Output";
+            this.Output.Size = new System.Drawing.Size(265, 27);
+            this.Output.TabIndex = 5;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(173, 131);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(94, 29);
+            this.button1.TabIndex = 6;
+            this.button1.Text = "Add (+)";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(284, 131);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(94, 29);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "Sub (-)";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(174, 166);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(94, 29);
+            this.button3.TabIndex = 8;
+            this.button3.Text = "Mul (*)";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(284, 166);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(94, 29);
+            this.button4.TabIndex = 9;
+            this.button4.Text = "Div (/)";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // clear_button
+            // 
+            this.clear_button.Location = new System.Drawing.Point(174, 315);
+            this.clear_button.Name = "clear_button";
+            this.clear_button.Size = new System.Drawing.Size(204, 29);
+            this.clear_button.TabIndex = 10;
+            this.clear_button.Text = "Clear";
+            this.clear_button.UseVisualStyleBackColor = true;
+            this.clear_button.Click += new System.EventHandler(this.clear_button_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(468, 376);
+            this.Controls.Add(this.clear_button);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.Output);
+            this.Controls.Add(this.Input2);
+            this.Controls.Add(this.Input1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximizeBox = false;
+            this.Name = "Form1";
+            this.Text = "Calculator";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Input1;
+        private System.Windows.Forms.TextBox Input2;
+        private System.Windows.Forms.TextBox Output;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button clear_button;
+    }
+}
+
---
+++ Calculator/Form1.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Calculator
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Output.Text = (Convert.ToInt32(Input1.Text) + Convert.ToInt32(Input2.Text)).ToString();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Output.Text = (Convert.ToInt32(Input1.Text) - Convert.ToInt32(Input2.Text)).ToString();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Output.Text = (Convert.ToInt32(Input1.Text) * Convert.ToInt32(Input2.Text)).ToString();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            Output.Text = (Convert.ToInt32(Input1.Text) / Convert.ToInt32(Input2.Text)).ToString();
+        }
+
+        private void clear_button_Click(object sender, EventArgs e)
+        {
+            Input1.Text = string.Empty;
+            Input2.Text = string.Empty;
+            Output.Text = string.Empty;
+        }
+
+        private void Input1_KeyPress(object sender, KeyPressEventArgs e)
+        {
+            e.Handled = !char.IsDigit(e.KeyChar);
+        }
+        
+    }
+}
